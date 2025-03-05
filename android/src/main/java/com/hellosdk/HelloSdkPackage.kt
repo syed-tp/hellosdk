8c0981821fd1ba75a5bc062cdc9e6a2809375fa3
syed-tp <syed@testpress.in>
2025-03-05 10:20:47 +0530

    add new streams player module

--- android/src/main/java/com/hellosdk/HelloSdkPackage.kt
+++ android/src/main/java/com/hellosdk/HelloSdkPackage.kt
@@ -8,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class HelloSdkPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(HelloSdkModule(reactContext))
+    return listOf(HelloSdkModule(reactContext), FragmentModule(reactContext))
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
